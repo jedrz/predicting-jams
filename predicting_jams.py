4941d24d0eecf46ffbba899678e8a1a64a5dca8d
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2016-04-02 18:35:39 +0200

    Parse street graph file

--- predicting_jams.py
+++ predicting_jams.py
@@ -1,5 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from collections import namedtuple
+
+STREET_GRAPH_PATH = 'data/street_graph.txt'
+
 def sample_function():
     return 42
+
+Point = namedtuple('Point', ['id', 'lat', 'long'])
+Edge = namedtuple('Edge', ['id1', 'id2', 'distance', 'lanes', 'max_velocity'])
+
+def parse_stree_graph():
+    with open(STREET_GRAPH_PATH) as f:
+        return parse_street_graph_lines(f.readlines())
+
+def parse_street_graph_lines(lines):
+    nodes = []
+    edges = []
+    for line in lines:
+        if not line or not line[0].isdigit():
+            continue
+        elements = line.strip().split(' ')
+        if len(elements) == 3:
+            nodes.append(parse_node(elements))
+        else:
+            edges.append(parse_edge(elements))
+    return (nodes, edges)
+
+def parse_node(elements):
+    return Point(id=int(elements[0]),
+                 lat=float(elements[1]),
+                 long=float(elements[2]))
+
+def parse_edge(elements):
+    return Edge(id1=int(elements[0]),
+                id2=int(elements[1]),
+                distance=float(elements[2]),
+                lanes=int(elements[3]),
+                max_velocity=int(elements[4]))
--- test_predicting_jams.py
+++ test_predicting_jams.py
@@ -5,3 +5,22 @@ from predicting_jams import *
 
 def test_sample_function():
     assert sample_function() == 42
+
+def test_parse_street_graph_lines():
+    input = '''
+Nodes:    
+Node_id Latitude Longitude  
+26063726 52.1528 21.0174  
+26063729 52.1424 21.0176  
+Edges:    
+Node1_id Node2_id Distance(km) Nr_of_lanes Avg_max_velocity
+26405552 26063726 0.022 2 70
+254508217 261239412 0.004 1 60
+'''
+
+    (nodes, edges) = parse_street_graph_lines(input.splitlines())
+
+    assert len(nodes) == 2
+    assert nodes[0] == (26063726, 52.1528, 21.0174)
+    assert len(edges) == 2
+    assert edges[0] == (26405552, 26063726, 0.022, 2, 70)
