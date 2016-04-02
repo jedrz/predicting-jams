6031def2e57a9bc55e1a7036cb2806a04ae125f5
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2016-04-02 15:35:31 +0200

    Init

---
+++ predicting_jams.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def sample_function():
+    return 42
---
+++ test_predicting_jams.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from predicting_jams import *
+
+def test_sample_function():
+    assert sample_function() == 42
