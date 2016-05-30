afa412ab3a07544a2607c0ab04b1e4f1b6f19b06
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2016-05-30 21:20:34 +0200

    Query similar segments

--- predicting_jams/db.py
+++ predicting_jams/db.py
@@ -8,3 +8,18 @@ def connect():
     conn_string = "host='localhost' port='25432' dbname='predicting-jams' user='docker' password='docker'"
     conn = psycopg2.connect(conn_string)
     return conn
+
+
+def _query(query_str, fetch_result_fn):
+    with connect() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(query_str)
+            return fetch_result_fn(cursor)
+
+
+def query_one(query_str):
+    return _query(query_str, lambda cursor: cursor.fetchone())
+
+
+def query_all(query_str):
+    return _query(query_str, lambda cursor: cursor.fetchall())
--- predicting_jams/predict.py
+++ predicting_jams/predict.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from collections import namedtuple
+
+from predicting_jams import db
+from predicting_jams.parse import Jam
+
+
+OrderedEdge = namedtuple('OrderedEdge', ['id1', 'id2', 'event_id'])
+
 
 def _precision(predicted, target, i):
     predicted = set(predicted[:i])
@@ -13,3 +21,46 @@ def quality(predicted, target):
     n = max(len(predicted), len(target))
     precisions = [_precision(predicted, target, i) for i in range(1, n + 1)]
     return sum(precisions) / n
+
+
+def get_random_example():
+    jam_id = db.query_one("SELECT id FROM jam_test ORDER BY random() LIMIT 1")[0]
+
+    def query_segments(table_name):
+        segments = db.query_all("""
+        SELECT node1_id, node2_id, event_id
+        FROM {}
+        WHERE jam_id = {}
+        ORDER BY event_id
+        """.format(table_name, jam_id))
+        return [OrderedEdge._make(segment) for segment in segments]
+
+    removed = query_segments('jam_removed')
+    in_20 = query_segments('jam_20m')
+    in_40 = query_segments('jam_40m')
+    return Jam(removed, in_20, in_40)
+
+
+def _query_geometry(node_id):
+    return db.query_one("SELECT ST_AsText(geometry) FROM node WHERE id = {}"
+                        .format(node_id))[0]
+
+# Looks like there is a lot of identical segments...
+def query_closest_segments(edge):
+    node1_geom = _query_geometry(edge.id1)
+    node2_geom = _query_geometry(edge.id2)
+    return db.query_all("""
+    SELECT
+      jam_20m_train.*,
+      (ST_Distance('SRID=4326;{node1_geom}'::geography, node1.geometry)
+        + ST_Distance('SRID=4326;{node2_geom}'::geography, node2.geometry)
+        + 0.1)
+        * power(abs(event_id - {event_id}), 2) as weight
+    FROM jam_20m_train
+    JOIN node node1 ON node1_id = node1.id
+    JOIN node node2 ON node2_id = node2.id
+    ORDER BY weight
+    LIMIT 10000
+    """.format(node1_geom=node1_geom,
+               node2_geom=node2_geom,
+               event_id=edge.event_id))
